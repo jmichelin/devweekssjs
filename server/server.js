3eaeadd2cce41c42a69ccfd2c93b0bb50ba3b9b5
jmichelin <jmichelinphone@gmail.com>
2017-06-17 17:40:52 -0400

    feat(files): serve file alienLandingPage.html

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// function readContent(callback) {
+//   fs.readFile("./Index.html", function (err, content) {
+//     if (err) return callback(err)
+//     callback(null, content)
+//   })
+// }
+//
+// readContent(function (err, content) {
+//   console.log(content)
+// })
+
+
+const readTemplate = (cb) => {
+  fs.readFile("./templates/alienLandingPage.html", 'utf8', (err, data) => {
+    if (err) return cb(err);
+    cb(null, data);
+  });
+};
+
 const server = http.createServer((req, res) => {
-  //res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Dev', 'week');
   res.writeHead(200);
-  res.end("Hello World\n");
+  readTemplate((err, data) => {
+    res.write(data)
+    res.end();
+  });
 });
 
 server.listen(port, hostname, () => {
