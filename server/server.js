cb93e05a593fa78fcf57ac39b70d1312a937e3c9
jmichelin <jmichelinphone@gmail.com>
2017-06-19 18:49:01 -0400

    style(routes): make route easier to see

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const handleMath = require('./utils/math');
 const express = require('express');
 const app = express();
 const hostname = '127.0.0.1';
@@ -7,10 +8,12 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
-const aliens = require('../server/utils/alienRoutes')
-
-app.use('/showResult', aliens);
-
+app.get('/showResult', function(req, res) {
+  let randomValue = handleMath.randomInt(0, 1);
+  let alienStatus = 'https://javascriptjohn.files.wordpress.com/2017/06/alivealien.png';
+  if(randomValue === 0) { alienStatus = 'https://javascriptjohn.files.wordpress.com/2017/06/sleepingalien.png' };
+  res.render('alienResults.ejs', {alienStatus: alienStatus});
+});
 
 app.get('/', function(req, res){
   res.render('alienLandingPage.ejs');
